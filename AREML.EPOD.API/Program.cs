cd7ee9cca2c0fe05970f4aceb3312a17dbb022c4
Gana80294 <gana@exalca.com>
2024-09-04 10:18:46 +0530

    UpdateQty

--- AREML.EPOD.API/Program.cs
+++ AREML.EPOD.API/Program.cs
@@ -12,6 +12,7 @@ using Newtonsoft.Json.Serialization;
 using AREML.EPOD.API.Auth;
 using AREML.EPOD.Data.Filters;
 using AREML.EPOD.Data.Helpers;
+using AREML.EPOD.Data.Logging;
 
 namespace AREML.EPOD.API
 {
@@ -27,7 +28,18 @@ namespace AREML.EPOD.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddCors(options => { options.AddPolicy("cors", a => a.WithOrigins("http://localhost:4200", "http://localhost:8100").AllowAnyHeader().AllowAnyMethod()); });
+
+            var corsUrls = builder.Configuration["Cors"]?.Split(",") ?? Array.Empty<string>();
+            LogWriter.WriteToFile(corsUrls.ToString());
+            builder.Services.AddCors(options => { options.AddPolicy("cors", a => a.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); });
+            //if (corsUrls.Length > 0) 
+            //{
+            //    builder.Services.AddCors(options => { options.AddPolicy("cors", a => a.WithOrigins(corsUrls).AllowAnyHeader().AllowAnyMethod()); });
+            //}
+            //else
+            //{
+            //    builder.Services.AddCors(options => { options.AddPolicy("cors", a => a.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); });
+            //}
 
             builder.Services.AddDbContext<AuthContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("DBContext"), sqlOptions =>
             {
@@ -37,31 +49,6 @@ namespace AREML.EPOD.API
 
             // Add Configuration Settings
             builder.Services.Configure<JwtSetting>(builder.Configuration.GetSection("JWTSecurity"));
-
-            // Add Authentication
-
-            //builder.Services.AddAuthentication(options =>
-            //{
-            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-            //}).AddJwtBearer(jwt =>
-            //{
-            //    jwt.RequireHttpsMetadata = false;
-            //    jwt.SaveToken = true;
-            //    jwt.TokenValidationParameters = new TokenValidationParameters
-            //    {
-            //        RequireExpirationTime = true,
-            //        ValidateLifetime = true,
-            //        RequireSignedTokens = true,
-            //        ValidateIssuerSigningKey = true,
-            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWTSecurity").GetValue<string>("securityKey"))),
-            //        ValidateIssuer = true,
-            //        ValidIssuer = builder.Configuration.GetSection("JWTSecurity").GetValue<string>("issuer"),
-            //        ValidateAudience = true,
-            //        ValidAudience = builder.Configuration.GetSection("JWTSecurity").GetValue<string>("audience")
-            //    };
-            //});
 
             builder.Services.AddControllers(config => { config.Filters.Add(typeof(CommonExceptionFilter)); }).AddJsonOptions(opt =>
             {
